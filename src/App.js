a376d9fb206d04ebf2ad6b649179c2b1df8cdf00
marco_dca <marcodecara@gmail.com>
2019-08-06 16:06:39 +0200

    twinkle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 import { geolocated } from "react-geolocated";
 
 function App(props) {
-  console.log(props)
-  const {coords} = props.coords ? props : {coords: {}}
+  console.log(props);
+  const { coords } = props.coords ? props : { coords: {} };
+  const { isGeolocationAvailable, isGeolocationEnabled, positionError } = props;
   return (
     <div>
       <p>Altitude: {coords.latitude}</p>
       <p>Longitude: {coords.longitude}</p>
+      <p>Geo available: {isGeolocationAvailable.toString()}</p>
+      <p>isGeolocationEnabled : {isGeolocationEnabled.toString()}</p>
+      <p>positionError : {String(positionError)}</p>
     </div>
   );
 }
 
 export default geolocated({
   positionOptions: {
-      enableHighAccuracy: false,
+    enableHighAccuracy: false
   },
-  userDecisionTimeout: 5000,
+  userDecisionTimeout: 5000
 })(App);
-
